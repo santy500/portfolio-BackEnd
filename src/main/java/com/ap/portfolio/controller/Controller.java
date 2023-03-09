dd59d77f737ec0cf0784ee76083eabb67aff9ba0
Santiago Torres <santivm400@gmail.com>
2023-03-08 23:35:16 -0300

    segunda actualizacion de cors

--- src/main/java/com/ap/portfolio/controller/Controller.java
+++ src/main/java/com/ap/portfolio/controller/Controller.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"https://portfolio-frontend-santiago.web.app"})
 public class Controller {
 
         
